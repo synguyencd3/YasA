8702ea227ae299a397dfdf096da7809cf433b898
synguyencd3 <52818031+synguyencd3@users.noreply.github.com>
2024-05-27 11:06:35 +0700

    fix color

--- storefront/src/component/categories.js
+++ storefront/src/component/categories.js
@@ -17,7 +17,7 @@ const CategoriesList = ({selectCategory}) => {
     return (
         <ol className="list-group list-group-numbered">
             {categories && categories.map((category) =>
-            <li className="list-group-item d-flex justify-content-between align-items-start" key={category.id} onClick={() =>{selectCategory(category)}}>
+            <li className="list-group-item d-flex justify-content-between align-items-start listCategory" key={category.id} onClick={() =>{selectCategory(category)}}>
                 <div className="ms-2 me-auto">
                     <div className="fw-bold">{category.name}</div>
                     {category.description} 
