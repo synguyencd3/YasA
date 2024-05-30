51719fb188bcfbdff5217f4f9b7522dfe948a1d6
synguyencd3 <52818031+synguyencd3@users.noreply.github.com>
2024-05-30 09:58:59 +0700

    login for admin

--- backoffice/src/App.js
+++ backoffice/src/App.js
@@ -3,12 +3,24 @@ import Header from './components/header/header';
 import Sidemenu from './components/sideMenu/sideMenu';
 import ProductCard from './components/pageContent/Product/productCard';
 import List from './components/pageContent/List';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import Login from './login/login';
+import { getToken } from './services/authService';
 
 function App() {
 
   let card = ProductCard
   let url =""
+
+  let [token, setToken] = useState();
+
+  useEffect(()=>{
+    setToken(getToken());
+}, [])
+
+  if(!token) {
+    return <Login setToken = {setToken}/>
+  }
 
   return (
     <div className="App">
---
+++ backoffice/src/login/login.js
@@ -0,0 +1,50 @@
+import { useState} from "react"
+import { setTokenToStorage } from "../services/authService";
+
+
+async function loginUser(credentials) {
+    return fetch('http://localhost:8080/api/auth/admin/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(credentials)
+    })
+        .then(data => data.json())
+   }
+
+const Login = ({setToken}) => {
+    const [username, setUserName] = useState();
+    const [password, setPassword] = useState();
+
+    const handleSubmit = async e => {
+        console.log(username);
+        console.log(password);
+        e.preventDefault();
+        const token = await loginUser({
+          username,
+          password
+        });
+        console.log(token);
+        setToken(token.accessKey);
+        setTokenToStorage(token.accessKey);
+      }
+
+    return(
+        <div className="login-wrapper">
+            <form onSubmit={handleSubmit}>
+            <div className="form-group">
+                <label for="exampleInputEmail1">Username</label>
+                <input onChange={e => setUserName(e.target.value)} className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter username"/>
+            </div>
+            <div className="form-group">
+                <label for="exampleInputPassword1">Password</label>
+                <input type="password" onChange={e => setPassword(e.target.value)} className="form-control" id="exampleInputPassword1" placeholder="Password"/>
+            </div>
+            <button type="submit" className="btn btn-primary">Submit</button>
+            </form>
+        </div>
+    )
+}
+
+export default Login;
